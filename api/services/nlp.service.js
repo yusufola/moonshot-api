adc8c4a8c4d7c93393190d83845facfa7f75643a
Yusuf Olalere <yusufola@localhost.localdomain>
2019-12-13 23:58:15 +0000

    email

--- api/services/nlp.service.js
+++ api/services/nlp.service.js
@@ -1,21 +1,22 @@
 const uuid = require('uuid');
 const fs = require('fs');
-const { Language, NlpManager } = require('node-nlp')
+const { NlpManager, ConversationContext } = require('node-nlp')
 
-const manager = new NlpManager({ languages: ['en'] });
-manager.container.register('fs', fs);
+const classifier = new NlpManager({ languages: ['en'] });
+const context = new ConversationContext()
+
 
 const nlpService = () => {
   const detectIntent = async (message) => {
       let result;
 
       if (fs.existsSync('./model.nlp')) {
-        await manager.load('./model.nlp');
+        await classifier.load('./model.nlp');
       }else{
         await train()
       }
 
-      result = await manager.process('en', message);
+      result = await classifier.process('en', message, context);
       console.log(message, result);
 
       return {response: result.answer}
@@ -24,25 +25,28 @@ const nlpService = () => {
   const train = async () => {
 
       // Adds the utterances and intents for the NLP
-      manager.addDocument('en', 'goodbye for now', 'greetings.bye');
-      manager.addDocument('en', 'bye bye take care', 'greetings.bye');
-      manager.addDocument('en', 'okay see you later', 'greetings.bye');
-      manager.addDocument('en', 'bye for now', 'greetings.bye');
-      manager.addDocument('en', 'i must go', 'greetings.bye');
-      manager.addDocument('en', 'hello', 'greetings.hello');
-      manager.addDocument('en', 'hi', 'greetings.hello');
-      manager.addDocument('en', 'howdy', 'greetings.hello');
+      classifier.addDocument('en', 'goodbye for now', 'greetings.bye');
+      classifier.addDocument('en', 'bye bye take care', 'greetings.bye');
+      classifier.addDocument('en', 'okay see you later', 'greetings.bye');
+      classifier.addDocument('en', 'bye for now', 'greetings.bye');
+      classifier.addDocument('en', 'i must go', 'greetings.bye');
+      classifier.addDocument('en', 'hello', 'greetings.hello');
+      classifier.addDocument('en', 'hi', 'greetings.hello');
+      classifier.addDocument('en', 'howdy', 'greetings.hello');
+      classifier.addDocument('en', '%email%', 'input.email');
       
  
-
       // Train also the NLG
-      manager.addAnswer('en', 'greetings.bye', 'Till next time');
-      manager.addAnswer('en', 'greetings.bye', 'see you soon!');
-      manager.addAnswer('en', 'greetings.hello', 'Hey there!');
-      manager.addAnswer('en', 'greetings.hello', 'Greetings!');
+      classifier.addAnswer('en', 'input.email', 'Greetings {{email}}');
+      classifier.addAnswer('en', 'greetings.bye', 'Till next time');
+      classifier.addAnswer('en', 'greetings.bye', 'see you soon!');
+      classifier.addAnswer('en', 'greetings.hello', 'Hey there!');
+      classifier.addAnswer('en', 'greetings.hello', 'Greetings!');
 
-    await manager.train();
-    await manager.save();
+
+
+    await classifier.train();
+    await classifier.save();
     return
   }
 
